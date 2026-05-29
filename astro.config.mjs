0e7e9b1b000c01ba2a3f1d00e35783e5b43f1926
shenlv7 <shenlv7@users.noreply.github.com>
2026-05-29 23:56:15 +0800

    fix: 正确配置GitHub Pages base路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://shenlv7.github.io/blog',
+  site: 'https://shenlv7.github.io',
+  base: '/blog',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
